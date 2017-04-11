0328c7947ef16887354fe3152df74e641c1dd43a
Gonzalo Gisbert <gonzalo.a.gisbert@gmail.com>
2017-04-11 15:38:01 -0300

    moved the Application class

--- src/main/java/com/gongon/analize/Application.java
+++ src/main/java/com/gongon/analize/Application.java
@@ -1,7 +1,14 @@
+package com.gongon.analize;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.service.ServiceRegistry;
+import com.gongon.analize.model.Country;
+
+import java.util.List;
 
 /**
  * Created by g0ng0n.
@@ -21,6 +28,29 @@ public class Application {
     public static void main (String[] args){
 
 
+        for(Country c : fetchAllContacts()){
+
+            System.out.println(c);
+        }
+
+    }
+
+
+
+    @SuppressWarnings("unchecked")
+    private static List<Country> fetchAllContacts(){
+        // Open session
+        Session session = sessionFactory.openSession();
+
+        // Create a criteria Object
+        Criteria criteria = session.createCriteria(Country.class);
+
+        // Get a List of countries objeccts according to the crietria object
+        List<Country> countries = criteria.list();
+
+        // Close session
+        session.close();
+        return countries;
     }
 
 
